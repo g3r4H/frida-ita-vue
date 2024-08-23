9fc09756d229657ebfb4f84152199d0016a87798
Gerardo R. Heredia <gerardo.heredia@protonmail.com>
2024-08-23 02:52:28 -0600

    Layout dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,21 +1,49 @@
 <template>
-  <div class="flex h-full flex-col gap-2 border-2 border-red-500">
-    <section class="flex-none border-2 border-green-500">
-      <div class="flex w-full flex-nowrap justify-between">
-        <coe-card name="Notify me" counter="5"> </coe-card>
-        <coe-card name="Notify me" counter="8"> </coe-card>
-        <coe-card name="Notify me" counter="0"> </coe-card>
-      </div>
+  <div class="flex h-full flex-col gap-2">
+    <!-- Navigation -->
+    <section class="flex-none">
+      <p class="text-center">Navigation</p>
     </section>
-    <section class="flex-1 border-2 border-blue-500">
-      Content
-      <section class="sidebar">
-        <coe-sidemenu></coe-sidemenu>
+
+    <!-- Page Content -->
+    <section class="flex flex-1 flex-nowrap gap-2">
+      <!-- Left Side -->
+      <section class="flex-none rounded-md border-2 border-gray-300 p-2">
+        <!-- Side menu -->
+        <section class="rounded-md">
+          <coe-sidemenu></coe-sidemenu>
+        </section>
       </section>
-      <main>
-        <section class="notifications"></section>
-        <section class="stepper"></section>
-        <section class="boxes"></section>
+
+      <!-- Right Side -->
+      <main class="flex flex-1 flex-col gap-2">
+        <!-- Notifications -->
+        <section
+          class="flex flex-nowrap justify-center gap-2 rounded-md border-2 border-gray-300 p-4"
+        >
+          <coe-card name="Notify me" counter="5"></coe-card>
+          <coe-card name="Notify me" counter="8"></coe-card>
+          <coe-card name="Notify me" counter="0"></coe-card>
+          <coe-card name="Notify me" counter="0"></coe-card>
+          <coe-card name="Notify me" counter="0"></coe-card>
+          <coe-card name="Notify me" counter="0"></coe-card>
+          <coe-card name="Notify me" counter="0"></coe-card>
+        </section>
+
+        <!-- Stepper -->
+        <section class="rounded-md border-2 border-gray-300">
+          <p class="text-center">Stepper</p>
+        </section>
+
+        <!-- Boxes -->
+        <section class="flex h-full flex-col gap-2 rounded-md border-2 border-gray-300 p-4">
+          <section class="flex-1 content-center justify-center border-2 border-gray-300">
+            <div class="text-center">Box 1</div>
+          </section>
+          <section class="flex-1 content-center justify-center border-2 border-gray-300">
+            <div class="text-center">Box 2</div>
+          </section>
+        </section>
       </main>
     </section>
   </div>
